d392ef81b5a485e8394842c8c8c1a84c6c8412e3
Sammy Straw <SStraw@carbonite.com>
2018-02-10 16:44:10 -0700

    draft 1

---
+++ assets/javascript/game.js
@@ -0,0 +1,33 @@
+// VARIABLES
+// ---------------------
+
+// There are a variety of words available
+
+// There is a score starting from zero
+
+// There are "tries" starting from 12 going down to 0
+
+
+
+// FUNCTIONS
+// ---------------------
+
+
+
+
+// MAIN GAME
+// ---------------------
+
+// Call function to begin the game
+
+// When player presses a key, it will run the following function
+
+// If you guess correctly, the letter(s) fill in the word
+
+// If you guess incorerctly, "tries" goes down by 1
+
+// If you guess the whole word, your score goes up by 1 and game ends
+
+// If "tries" reaches 0, you lose and game ends
+
+// When game ends a new word loads automatically
