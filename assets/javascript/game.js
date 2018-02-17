d11e6eca443aa73bc87eaba5d7960981b3373648
Sammy Straw <SStraw@carbonite.com>
2018-02-16 20:29:18 -0700

    another draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,5 @@
 console.log("hey girl chill out, you can do this ok?");
 
-console.log(event)
-
-
 // List of words for game
 var hangmanWords = ["apple", "persimmon", "apricot", "banana", "watermelon"];
 
@@ -13,73 +10,76 @@ var word = hangmanWords[Math.floor(Math.random() * hangmanWords.length)];
 var score = 0;
 
 // Starting try number
-var tries = 10;
+var tries = 5;
 
 // Word translated into blanks to be solved
 var underScore = [];
-    for (i = 0; i < word.length; i++) {
-        underScore[i] = "_" + " ";
+    for (j = 0; j < word.length; j++) {
+        underScore[j] = "_" + " ";
     };
 
-// Remaining letters
-var remainingLetters = word.length;
-
-
 // HTML ID for blank word
-document.getElementById("wordlist").innerHTML = underScore.join(" ");
+document.getElementById("blankword").innerHTML = underScore.join(" ");
+
 
 // User input gathered for a letter guess
 document.onkeypress = function(event){
-    var letterGuess = String.fromCharCode(event.which).toLowerCase();
+    var letterGuess = event.key;
     console.log(letterGuess);
-    // Check letterGuess (event) against each letter in random word
-    for (var x = 0; x < word.length; x++){  
-        if (word[x] === letterGuess) {
-            console.log("Reached here");
-            remainingLetters--;
-            wordUpdate();
-            hasLettersRemaining();
-            youWin();
+    
+    // Checks letters against word and updates underscores
+        function wordUpdate() {   
+            if (word.indexOf(letterGuess) !== -1) {
+                for (var i = 0; i < underScore.length; i++) {
+                if (letterGuess === word[i]) {
+                underScore[i] = letterGuess;
+                document.getElementById("blankword").innerHTML = underScore.join(" ");
+                    }
+                }
+            } 
+            
+            // This is if the guess is wrong
+            else {
+                    document.getElementById("tryagain").innerHTML = "Tries left: " + tries;
+                    console.log("That letter is not correct");
+                    tries--;
+                    }
+        };
+    
+
+    wordUpdate();
+    youLose();
+    youWin();
+};
+
+// This will occur if you guessed the word!
+function youWin() {
+    if (underScore.join("") == word) {
+        console.log("You win!");
+        document.getElementById("winlose").innerHTML = "You win!";
+        scoreUpdate();
+        // loadWord();
         }
-        //debug: how to only run if "if" is false
-        else {
-            console.log("You have " + tries + " tries left")
-            hasLettersRemaining();
-            tries--;
-        }
-    }
+    };
+
+// This will update the score on the HTML
+
+function scoreUpdate() {
+    score++
+    document.getElementById("score").innerHTML = "SCORE: " + score;
 };
 
 // This will bring a new word up on the screen
 function loadWord() {
-
-};
-
-// This will update the underscores into letters
-function wordUpdate() {
-
-};
-
-// This will check if there are letters remaining
-function hasLettersRemaining() {
-    if (remainingLetters > 0) {
-        return;
-    }
-};
-
-// This will let you know you win, update the score, and load a new word.
-function youWin() {
-    if (remainingLetters < 1) {
-        console.log("You win!")
-        score++
-        // loadWord();
-    }
+            document.getElementById("blankword").reset();
 };
 
 // This will let you know you lose and load a new word.
 function youLose() {
-    if (tries < 1) {
+    if (tries < 0) {
         console.log("You lose!")
+        document.getElementById("winlose").innerHTML = "You lose!"
         // loadWord();
     }
 };
+
